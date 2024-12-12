44c6db3464b0d4cb399cbfa7d44290d3051e4c66
Ahmed Hassan <106224867+Capa11@users.noreply.github.com>
2024-12-12 09:22:30 +0200

    example for using new interface
    only for learning how to use not else

--- chickenEggs/SimpleGLEventListener.java
+++ chickenEggs/SimpleGLEventListener.java
@@ -2,6 +2,12 @@ package chickenEggs;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
+<<<<<<< Updated upstream
+=======
+import chickenEggs.objects.Game.*;
+import chickenEggs.interfaces.*;
+import chickenEggs.objects.Pages.CustomScreen;
+>>>>>>> Stashed changes
 
 import chickenEggs.interfaces.*;
 import chickenEggs.objects.*;
@@ -13,6 +19,7 @@ public class SimpleGLEventListener extends variables {
     public void init(GLAutoDrawable gld) {
         gl = gld.getGL();
         init(gl);
+<<<<<<< Updated upstream
         mons = new monst(300,0,200,200);
         str1 = new drawable[20];
         for (int i = 0; i < 20; i++) {
@@ -26,14 +33,21 @@ public class SimpleGLEventListener extends variables {
             str[i].path=baskets[1];
         }
         initGrid(str,-xaxis,xaxis,yaxis,100,100,20,20);
+=======
+        runningPage=new CustomScreen();
+>>>>>>> Stashed changes
     }
     public void display(GLAutoDrawable gld) {
         gl = gld.getGL();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
         drawBackground();
+<<<<<<< Updated upstream
         mons.draw();
         drawArray(str1);
         drawArray(str);
+=======
+        runningPage.draw();
+>>>>>>> Stashed changes
 
     }
     void drawBackground(){
