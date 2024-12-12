fcf6959ee28db59da1dd81e31c1a810aa73f18b4
Ahmed Hassan <106224867+Capa11@users.noreply.github.com>
2024-12-12 15:20:11 +0200

    pull from main

--- chickenEggs/objects/Pages/HomeScreen.java
+++ chickenEggs/objects/Pages/HomeScreen.java
@@ -9,14 +9,18 @@ import static chickenEggs.interfaces.variables.*;
 
 public class HomeScreen extends drawable{
     drawable[] arr =  initwriteString("hazem" , 50 , 200 , -500 ,10 , 10 , 2 );
-
     void drawBackground(){
         DrawSprite(0,0, (int) (xaxis*2), (int) (yaxis*2), backgrounds[0]);
         DrawSprite(0,0, (1200), (500), backgrounds[1]);
     }
+<<<<<<< Updated upstream
     drawable[] button(String str , float ys, int x , int y , int gabY , float Bw , float Bh , int w , int h , int texture ){
         drawable ha = new drawable(50 ,100 , 500 , 200 , numbers[2]);
        return initwriteString(str , x - (Bw/2) , x + (Bw/2) , y - (Bh/2),w,h, gabY);
+=======
+    drawable[] button(String str , float ys, int x , int y , int gabY , float Bw , float Bh , int w , int h ){
+       return initwriteString(str , x - (Bw/2) , x + (Bw/2) , y - (Bh/2),w,h, gabY );
+>>>>>>> Stashed changes
     }
     public HomeScreen() {
     }
