c4f7f67173b9a84f69effc2e3e1fd4a2160f2020
Ahmed Hassan <106224867+Capa11@users.noreply.github.com>
2024-12-13 11:35:40 +0200

    fixing bugs came from merging

--- chickenEggs/SimpleGLEventListener.java
+++ chickenEggs/SimpleGLEventListener.java
@@ -22,72 +22,14 @@ public class SimpleGLEventListener extends variables {
     public void init(GLAutoDrawable gld) {
         gl = gld.getGL();
         init(gl);
-        runningPage=new CustomScreen();
-
-    UnordinaryChicken chicken1;
-    OrdinaryChicken chicken;
-    SuperChicken chicken2;
-    UltimateChicken chicken3;
-    eggs egg;
-    public void init(GLAutoDrawable gld) {
-        gl = gld.getGL();
-        init(gl);
-        mons = new monst(300,0,200,200);
-        str1 = new drawable[20];
-        for (int i = 0; i < 20; i++) {
-            str1[i] = new drawable();
-            str1[i].path=baskets[0];
-        }
-        initGrid(str1,-xaxis,xaxis,0,300,100,20,20);
-        str = new drawable[20];
-        for (int i = 0; i < 20; i++) {
-            str[i] = new drawable();
-            str[i].path=baskets[1];
-        }
-        initGrid(str,-xaxis,xaxis,yaxis,100,100,20,20);
-         chicken = new OrdinaryChicken(0,0,200,200,3,-1000,1000,-600,600);
-        chicken1 = new UnordinaryChicken(-200,0,200,200,3,-1000,1000,-600,600);
-        chicken2 = new SuperChicken(-400,0,200,200,3,-1000,1000,-600,600);
-        chicken3 = new UltimateChicken(-600,0,200,200,3,-1000,1000,-600,600);
+        runningPage = new CustomScreen();
 
     }
+
     public void display(GLAutoDrawable gld) {
         gl = gld.getGL();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-
         runningPage.draw();
-
-
-
-        drawBackground();
-        chicken.draw();
-        chicken1.draw();
-        chicken2.draw();
-        chicken3.draw();
-        chicken.falleggs();
-        chicken1.falleggs();
-        chicken2.falleggs();
-        chicken3.falleggs();
-        for (int i = 0; i < chicken.eggs.size(); i++) {
-            if(!(chicken.eggs.get(i).is_inside())){
-                chicken.eggs.remove(i);
-            }
-        }
-        for (int i = 0; i < chicken1.eggs.size(); i++) {
-            if(!(chicken1.eggs.get(i).is_inside())){
-                chicken1.eggs.remove(i);
-            }
-        }
-        for (int i = 0; i < chicken2.eggs.size(); i++) {
-            if(!(chicken2.eggs.get(i).is_inside())){
-                chicken2.eggs.remove(i);
-            }
-        }
-        for (int i = 0; i < chicken3.eggs.size(); i++) {
-            if(!(chicken3.eggs.get(i).is_inside())){
-                chicken3.eggs.remove(i);
-            }
-        }
 
     }
 
