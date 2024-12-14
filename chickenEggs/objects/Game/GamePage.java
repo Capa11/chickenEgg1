9c88e0d2e1cdde61212df09fd9f85ea17b626379
omarHassan <jadm7341@gmail.com>
2024-12-14 23:00:18 +0200

    printing scores of the players on the top of the screen

--- chickenEggs/objects/Game/GamePage.java
+++ chickenEggs/objects/Game/GamePage.java
@@ -17,13 +17,14 @@ import static chickenEggs.interfaces.variables.bullets;
 
 public class GamePage extends Page {
     float[] probilityChickens;
-    public int howManyPlayers,lives=5;int[] score;
+    public int howManyPlayers,lives=5;
+    ArrayList<drawable[]> scores;
     public mousePlayer mousePlayer;
     public boolean isDead=false;
     public ArrayList<Egg> eggs=new ArrayList<>();
     public ArrayList<Chicken> chickens=new ArrayList<>();
     public ArrayList<bullet> bullets=new ArrayList<>(0);
-    public ArrayList<keyPlayer> keyPlayers;
+    public ArrayList<keyPlayer> keyPlayers = new ArrayList<>();
     public ArrayList<AiPlayer> AiPlayers;
     public ArrayList<Player> players = new ArrayList<>();;
     public GamePage(mousePlayer mousePlayer, ArrayList<keyPlayer> keyPlayers, ArrayList<AiPlayer> AiPlayers, int level){//level 0 means not custom game
@@ -34,10 +35,10 @@ public class GamePage extends Page {
         if(mousePlayer!=null)players.add(mousePlayer);
         players.addAll(keyPlayers);
         players.addAll(AiPlayers);
-
+        scores = new ArrayList<drawable[]>();
     }
     public ArrayList<Chicken> initChicken(int level){
-       chickens = new ArrayList<>(20);//how many chickens
+        chickens = new ArrayList<>(20);//how many chickens
         for (int i = 0; i < chickens.size(); i++) {
             chickens.set(i,new Chicken(0,0,0,0,0,0,0));
         }
@@ -64,7 +65,7 @@ public class GamePage extends Page {
     @Override
     public void draw(){
         super.draw();
-         moveAll();
+        moveAll();
         for (int i = 0; i < chickens.size(); i++) {
             chickens.get(i).draw();
         }
@@ -98,7 +99,7 @@ public class GamePage extends Page {
     public void mouseClicked(){
         if(isClickInside()&&mousePlayer!=null) {
             System.out.println("click");
-                mousePlayer.mouseClicked();
+            mousePlayer.mouseClicked();
 
         }
     }
@@ -111,6 +112,29 @@ public class GamePage extends Page {
         Chicken[] chickens1 = (Chicken[]) chickens.toArray();
         for (int i = 0; i < chickens.size(); i++) {
             initGrid(chickens1,-xaxis,xaxis,-yaxis,Chicken.defaultSize,Chicken.defaultSize,20,20);
+        }
+    }
+    private void initScores(){
+        int xstart = (int)-xaxis + 100;
+        int ystart = (int)yaxis-80;
+        int y = ystart;
+        for (int i = 0; i < players.size(); i++) {
+            String s = "" + players.get(i).score;
+            scores.add(new drawable[s.length()]);
+            int x = xstart;
+            for (int j = 0; j < s.length(); j++) {
+                scores.get(i)[j] = new drawable(x , y , 60 , 60 ,numbers[s.charAt(j) - '0']);
+                x+=70;
+            }
+            y-=70;
+        }
+    }
+    public void draw_score(){
+        initScores();
+        for (int i = 0; i < scores.size(); i++) {
+            for (int j = 0; j < scores.get(i).length; j++) {
+                if(scores.get(i)!=null && scores.get(i)[j]!=null)scores.get(i)[j].draw();
+            }
         }
     }
 }
