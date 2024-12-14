abe47287f8926eebe7fb896729a2f766e9796a13
youssef <yousseef.ah@gmail.com>
2024-12-14 19:32:12 +0200

    adding the custom cursor

--- chickenEggs/SimpleGLEventListener.java
+++ chickenEggs/SimpleGLEventListener.java
@@ -8,7 +8,12 @@ import chickenEggs.interfaces.*;
 
 import chickenEggs.interfaces.Pages.*;
 import chickenEggs.objects.Pages.*;
-
+//cursor
+import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+//end
 
 public class SimpleGLEventListener extends variables {
     drawable mons;
@@ -26,10 +31,32 @@ public class SimpleGLEventListener extends variables {
 //        runningPage = new Statement();
 //        runningPage = new HandelChickenMove();
 //        runningPage = new InstractionDefult();
-//        runningPage = new chickencircle();
-        runningPage = new chickencircle2();
+        runningPage = new chickencircle();
+        // runningPage = new chickencircle2();
+
+        // Set custom cursor
+        setCustomCursor(gld);
 
     }
+//cursor
+    private void setCustomCursor(GLAutoDrawable gld) {
+        // Get the AWT component from the GL drawable
+        java.awt.Component component = (java.awt.Component) gld;
+
+        // Load the cursor image
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image cursorImage = toolkit.getImage("chickenEgg1/chickenEggs/Assets/Man/Man2.png");
+        //chickenEggs/Assets/Man/Man2.png
+        //D:\practice code\304 file\project-game-jetbrains--main\chickenEgg1\chickenEggs\Assets\Man\Man2.png
+
+        // Create the custom cursor
+        Cursor customCursor = toolkit.createCustomCursor(cursorImage, new Point(0, 0), "Custom Cursor");
+
+        // Set the custom cursor on the component
+        component.setCursor(customCursor);
+    }
+    //end
+
     public void display(GLAutoDrawable gld) {
         gl = gld.getGL();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
